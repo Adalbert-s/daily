e8ed1375d303ea2e4e860596976675641599d3e0
uzuki <adalbertosantospinheiro@outlook.com>
2024-10-13 19:16:47 -0300

    bug corrigido register

--- daily/users/views.py
+++ daily/users/views.py
@@ -1,9 +1,12 @@
 
 # Create your views here.
 
+from imaplib import _Authenticator
+from multiprocessing import AuthenticationError
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from .forms import RegistrationForm
+from django.contrib.auth.forms import AuthenticationForm
 
 def register(request):
     if request.method == 'POST':
@@ -11,10 +14,19 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)  # Log in the user immediately after registration
-            return redirect('dashboard')  # Redirect to home after successful registration
+            return redirect(request.POST.get('next', 'dashboard_home'))  # Redirect to home after successful registration
     else:
         form = RegistrationForm()
     return render(request, 'users/register.html', {'form': form, 'title': 'Register'})
 
-def dashboard_home(request):
-    return render(request, 'dashboard/home.html', {'title': 'Dashboard'}) 
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationError(request, data=request.POST)
+        if form.is_valid():
+            user = _Authenticator(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            if user is not None:
+                login(request, user)
+                return redirect(request.POST.get('next', 'dashboard'))   
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form, 'title': 'Log in'})
