db1de4ed9badf8c619aa6c91b329515002d4b658
uzuki <adalbertosantospinheiro@outlook.com>
2024-11-17 11:04:11 -0300

    bug AUTH_USER arrumado

--- daily/dashboard/models.py
+++ daily/dashboard/models.py
@@ -1,13 +1,11 @@
-# dashboard/models.py
-
+from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User
 
 class Note(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user.username} - {self.text[:20]}"
-
+        return self.title
