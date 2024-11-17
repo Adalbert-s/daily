b7c693a9ba85696f102e7ec07ebcdf5d0b05163e
uzuki <adalbertosantospinheiro@outlook.com>
2024-11-17 11:33:40 -0300

    erro register corrigido

--- daily/users/views.py
+++ daily/users/views.py
@@ -1,17 +1,18 @@
+# users/views.py
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages
 from django.contrib.auth import login
+from django.contrib import messages
+from .forms import CustomUserCreationForm  # Importe o formulário aqui
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)  # Faz login automático do usuário
+            user = form.save()  # Salva o usuário
+            login(request, user)  # Faz login automático
             messages.success(request, 'Cadastro realizado com sucesso!')
-            return redirect('dashboard_home')  # ou para a página que preferir
+            return redirect('dashboard_home')  # Redireciona para o dashboard
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
 
     return render(request, 'users/register.html', {'form': form, 'title': 'Cadastro'})
