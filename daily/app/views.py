fc72997aae31f33b9abeafa58a5674aad6e7cc80
Diogo-AB <diogoazevedobataginii@gmail.com>
2024-11-18 11:46:34 -0300

    Botão para página cadastro

--- daily/app/views.py
+++ daily/app/views.py
@@ -6,6 +6,12 @@ from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpRequest
 
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.http import HttpRequest
+from datetime import datetime
+
 def home(request):
     """Renders the home page."""
     assert isinstance(request, HttpRequest)
@@ -17,3 +23,17 @@ def home(request):
             'year':datetime.now().year,
         }
     )
+
+def register(request):
+    """Handles the user registration."""
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Loga o usuário automaticamente após o registro
+            return redirect('home')  # Redireciona para a página inicial ou outra página
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'users/register.html', {'form': form})
+
