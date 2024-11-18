ecddee57a50d85f7be21f72bc5256031dfc4e390
Diogo-AB <diogoazevedobataginii@gmail.com>
2024-11-18 11:16:46 -0300

    Login dashboard

--- daily/daily/urls.py
+++ daily/daily/urls.py
@@ -32,5 +32,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('admin/', admin.site.urls),
 
-    path('dashboard/', include("dashboard.urls"))
+    path('dashboard/', include("dashboard.urls")),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('', include('users.urls')),
 ]
