14a8453d0db49d8add3c4f4c1eed5339e96d6fa8
Diogo <117372616+Diogo-AB@users.noreply.github.com>
2025-04-25 11:29:34 -0300

    Create TodoNota.cs

---
+++ TodoApi/TodoApi/Models/TodoNota.cs
@@ -0,0 +1,11 @@
+namespace TodoApi.Models
+{
+    public class TodoNota
+    {
+        public int Id { get; set; }
+        public string? Titulo { get; set; }
+        public string? Descricao { get; set; }
+        public string? Data {  get; set; }
+        public string? Hora { get; set; }
+    }
+}
